00528e27ebb08ceb0bba96c4177f4089d33f427a
Hythem-shaban <hythem.hm29@gmail.com>
2024-02-19 21:29:09 +0200

    update mul_div top

--- src/riscv_core_mul_div.sv
+++ src/riscv_core_mul_div.sv
@@ -9,8 +9,11 @@ module riscv_core_mul_div
   input   logic            i_mul_div_en,
   input   logic            i_mul_div_isword,
   input   logic            i_mul_div_clk,
-  input   logic            i_mul_div_rstn,   
+  input   logic            i_mul_div_rstn,
+  output  logic            o_mul_div_busy,   
   output  logic            o_mul_div_done,
+  output  logic            o_mul_div_overflow,
+  output  logic            o_mul_div_div_by_zero,
   output  logic [XLEN-1:0] o_mul_div_result 
 );
 
@@ -43,7 +46,10 @@ u_riscv_core_div
   .i_div_en(i_mul_div_control[2] & i_mul_div_en),
   .i_div_clk(i_mul_div_clk),
   .i_div_rstn(i_mul_div_rstn),
+  .o_div_busy(o_mul_div_busy),
   .o_div_done(o_mul_div_done),
+  .o_div_overflow(o_mul_div_overflow),
+  .o_div_div_by_zero(o_mul_div_div_by_zero),
   .o_div_result(div_result)
 );
 
