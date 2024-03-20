90d367ae29f0dc841fb3f793145ab0e1f98ca9ee
AKhaled47 <alikhaled7237@gmail.com>
2024-03-21 01:15:46 +0200

    CSR_added

--- src/riscv_core_main_decoder.sv
+++ src/riscv_core_main_decoder.sv
@@ -20,7 +20,9 @@ module riscv_core_main_decoder (
     output logic       o_main_decoder_amo,
     output logic [3:0] o_main_decoder_amo_op,
     output logic       o_main_decoder_lr,
-    output logic       o_main_decoder_sc
+    output logic       o_main_decoder_sc,
+    output logic       o_main_decoder_src_sel,
+    output logic [1:0] o_main_decoder_op
 
 );
 
@@ -105,6 +107,8 @@ case (i_main_decoder_opcode)
    7'b1100111:  control_signals = 18'b1_000_0_1_0_10_0_0_00_0_0_1_1_0; // jalr
    7'b0110111:  control_signals = 18'b1_100_1_1_0_00_0_0_00_0_0_0_0_0; // lui
    7'b0010111:  control_signals = 18'b1_100_0_1_0_00_0_0_00_0_0_0_0_0; // auipc
+   7'b1110011:  control_signals = 18'b1_110_0_0_0_11_0_0_00_0_0_0_0_0; // CSR_instruction
+
 
    7'b0101111:  begin // A extension 
                      control_signals = 18'b1_101_0_1_1_01_0_0_10_0_0_0_0_0;
@@ -155,6 +159,7 @@ case (i_main_decoder_opcode)
    7'b0110111:   o_main_decoder_new_mux_sel = 1; // lui
    7'b0010111:   o_main_decoder_new_mux_sel = 1; // auipc
    7'b0101111:   o_main_decoder_new_mux_sel = 0; // A extension
+   7'b1110011:   o_main_decoder_new_mux_sel = 0; // CSR_instruction
 
     default   :  o_main_decoder_new_mux_sel = 0; // Default Case     
 endcase
@@ -198,6 +203,53 @@ endcase
 
 end
 
+
+
+//////////////////////
+/// SRC_SEL LOGIC ////
+//////////////////////
+
+always_comb begin : SRC_SEL_LOGIC
+
+o_main_decoder_src_sel = 0;
+
+case (i_main_decoder_opcode)
+
+    7'b1110011: begin
+                o_main_decoder_src_sel = 0;
+                if(i_main_decoder_funct3[2])
+                o_main_decoder_src_sel = 1;
+                end
+
+    default: o_main_decoder_src_sel = 0 ;
+endcase
+
+end
+
+
+/////////////////
+/// OP LOGIC ////
+/////////////////
+
+always_comb begin : OP_LOGIC
+
+o_main_decoder_op = 2'b00;
+
+case (i_main_decoder_opcode)
+
+    7'b1110011: begin
+               case (i_main_decoder_funct3[1:0])
+                2'b01  : o_main_decoder_op = 2'b01; 
+                2'b10  : o_main_decoder_op = 2'b10; 
+                2'b11  : o_main_decoder_op = 2'b11; 
+                default: o_main_decoder_op = 2'b00; 
+               endcase
+                end
+
+    default: o_main_decoder_op = 2'b00; 
+endcase
+
+end
 
 
 endmodule
