729aa53e6a1eadae4db17040a8fddcc210893e03
Hythem Ahmed <89210658+Hythem-shaban@users.noreply.github.com>
2024-08-07 17:07:41 +0300

    update mul_in and div_in

--- RTL/riscv_core_div_in.sv
+++ RTL/riscv_core_div_in.sv
@@ -14,6 +14,12 @@ module riscv_core_div_in
 logic [1:0] srcA_srcB_sign;             // sign-bit of dividend & divisor
 logic [1:0] srcA_srcB_word_sign;        // sign-bit of w_dividend & w_divisor
 
+logic [XLEN-1:0] srcA_comp;
+logic [XLEN-1:0] srcB_comp;
+
+logic [XLEN-1:0] wsrcA_comp;
+logic [XLEN-1:0] wsrcB_comp;
+
 localparam [1:0] DIV    = 2'b00;        
 localparam [1:0] DIVU   = 2'b01;        
 localparam [1:0] REM    = 2'b10;        
@@ -26,6 +32,12 @@ localparam [1:0] REMUW  = 2'b11;
 
 assign srcA_srcB_sign = {i_div_in_srcA[XLEN-1], i_div_in_srcB[XLEN-1]};
 assign srcA_srcB_word_sign = {i_div_in_srcA[XLEN/2-1], i_div_in_srcB[XLEN/2-1]};
+
+assign srcA_comp = ~i_div_in_srcA + 1'b1;
+assign srcB_comp = ~i_div_in_srcB + 1'b1;
+
+assign wsrcA_comp = ~i_div_in_srcA[XLEN/2-1:0] + 1'b1;
+assign wsrcB_comp = ~i_div_in_srcB[XLEN/2-1:0] + 1'b1;
 
 always_comb
   begin: instr_proc
@@ -43,17 +55,17 @@ always_comb
                 2'b01:
                   begin
                     o_div_in_dividend = i_div_in_srcA;
-                    o_div_in_divisor = ~i_div_in_srcB + 1;
+                    o_div_in_divisor = srcB_comp;
                   end
                 2'b10:
                   begin
-                    o_div_in_dividend = ~i_div_in_srcA + 1;
+                    o_div_in_dividend = srcA_comp;
                     o_div_in_divisor = i_div_in_srcB;
                   end
                 2'b11:
                   begin
-                    o_div_in_dividend = ~i_div_in_srcA + 1;
-                    o_div_in_divisor = ~i_div_in_srcB + 1;  
+                    o_div_in_dividend = srcA_comp;
+                    o_div_in_divisor = srcB_comp;  
                   end
                 default: 
                   begin
@@ -78,17 +90,17 @@ always_comb
                 2'b01:
                   begin
                     o_div_in_dividend = i_div_in_srcA;
-                    o_div_in_divisor = ~i_div_in_srcB + 1;
+                    o_div_in_divisor = srcB_comp;
                   end
                 2'b10:
                   begin
-                    o_div_in_dividend = ~i_div_in_srcA + 1;
+                    o_div_in_dividend = srcA_comp;
                     o_div_in_divisor = i_div_in_srcB;
                   end
                 2'b11:
                   begin
-                    o_div_in_dividend = ~i_div_in_srcA + 1;
-                    o_div_in_divisor = ~i_div_in_srcB + 1;   
+                    o_div_in_dividend = srcA_comp;
+                    o_div_in_divisor = srcB_comp;   
                   end
                 default:
                   begin
@@ -122,18 +134,18 @@ always_comb
                   end
                 2'b01:
                   begin
-                    o_div_in_dividend = i_div_in_srcA;
-                    o_div_in_divisor = ~i_div_in_srcB + 1;
+                    o_div_in_dividend = i_div_in_srcA[XLEN/2-1:0];
+                    o_div_in_divisor = wsrcB_comp;
                   end
                 2'b10:
                   begin
-                    o_div_in_dividend = ~i_div_in_srcA + 1;
-                    o_div_in_divisor = i_div_in_srcB;
+                    o_div_in_dividend = wsrcA_comp;
+                    o_div_in_divisor = i_div_in_srcB[XLEN/2-1:0];
                   end
                 2'b11:
                   begin
-                    o_div_in_dividend = ~i_div_in_srcA + 1;
-                    o_div_in_divisor = ~i_div_in_srcB + 1;  
+                    o_div_in_dividend = wsrcA_comp;
+                    o_div_in_divisor = wsrcB_comp;  
                   end
                 default: 
                   begin
@@ -157,18 +169,18 @@ always_comb
                   end
                 2'b01:
                   begin
-                    o_div_in_dividend = i_div_in_srcA;
-                    o_div_in_divisor = ~i_div_in_srcB + 1;
+                    o_div_in_dividend = i_div_in_srcA[XLEN/2-1:0];
+                    o_div_in_divisor = wsrcB_comp;
                   end
                 2'b10:
                   begin
-                    o_div_in_dividend = ~i_div_in_srcA + 1;
-                    o_div_in_divisor = i_div_in_srcB;
+                    o_div_in_dividend = wsrcA_comp;
+                    o_div_in_divisor = i_div_in_srcB[XLEN/2-1:0];
                   end
                 2'b11:
                   begin
-                    o_div_in_dividend = ~i_div_in_srcA + 1;
-                    o_div_in_divisor = ~i_div_in_srcB + 1;  
+                    o_div_in_dividend = wsrcA_comp;
+                    o_div_in_divisor = wsrcB_comp;  
                   end
                 default:
                   begin
